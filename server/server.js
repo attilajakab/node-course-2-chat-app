9e0e9b4c013de12d6b7fc3685232be70e579b5a1
Attila Jakab <attila.jakab@globallogic.com>
2017-12-03 20:31:06 +0100

    Format timestamps using momentjs

--- server/server.js
+++ server/server.js
@@ -18,14 +18,11 @@ app.use(express.static(publicPath));
 // io.emit - emits message to every connetction
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
-        console.log('Create message', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
     });
